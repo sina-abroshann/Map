76aa3ed81ae7c497bce033723e25dcd6d9b00b4d
Sina Abroshan <52177002+sina-abroshann@users.noreply.github.com>
2023-01-18 10:21:37 +0330

    Update settings.py

--- map/settings.py
+++ map/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p^&=ifx$_gop-#99mlqzw)hj-9$-a4jw7c&algz733@3*0!)_^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
